11aedb23055278a6b9d4df498a6e10d8241d8cad
Lindsey Whitmore <lwhitm1@gmail.com>
2023-09-13 13:05:07 -0600

    updated print_fires doc to run with my_utils

--- print_fires.py
+++ print_fires.py
@@ -3,5 +3,8 @@ country='United States of America'
 country_column = 0
 fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
+"""fires_column is set to default value of 1. If you wish to 
+return another column explicitly state what column you wish 
+to use when utilizing the function"""
 fires = get_column(file_name, country_column, country, fires_column)
 print(fires)
