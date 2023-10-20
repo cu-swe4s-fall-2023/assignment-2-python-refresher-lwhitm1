1af55ebd2ef97596889657a9715aa89bfb596b2d
lwhitm1 <lwhitm1@gmail.com>
2023-10-20 20:00:35 +0000

    updating to pass ci tests

--- src/hist.py
+++ src/hist.py
@@ -1,18 +1,20 @@
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
 
 data_file = sys.argv[1]
 out_file = sys.argv[2]
 title = sys.argv[3]
-x=sys.argv[4]
-y=sys.argv[5]
+x = sys.argv[4]
+y = sys.argv[5]
 
 D = []
-for l in open(data_file):
-    D.append(float(l))
+with open(data_file, 'r') as file:
+    for line in file:
+        D.append(float(line))
 
 fig, ax = plt.subplots()
 ax.hist(D)
@@ -22,4 +24,4 @@ ax.set_xlabel(x)
 ax.set_ylabel(y)
 ax.set_title(title)
 
-plt.savefig(out_file,bbox_inches='tight')
+plt.savefig(out_file, bbox_inches='tight')
